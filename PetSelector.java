3d68a8ec7fbcd90761b556d8b0e7eaf70c90c226
Toon Paul <hudallap@gmail.com>
2025-10-13 18:28:01 +0000

    save

--- PetSelector.java
+++ PetSelector.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public class PetSelector{
+    public static void main (String [] args){
+        //ask the user to input (name, season, and color)
+        Scanner userInput = new Scanner(System.ln);
+        System.out.println("Enter your favorite color (red, green, or blue)");
+        String color = scan.NextLine();
+        String season;
+        String name;
+        if("abcdefghijklmnopqrstuvwxyz".indexOf(color.substring(i, i+1).toLowerCase()) >= 0){
+            System.out.println("Enter your favorite season (winter, summer, spring, fall)");
+            season = scan.NextLine();
+        }
+        //make sure the input is valid (Idiot proof)
+        //make a decision
+        //tell the user waht they get
+    }
 
+}
