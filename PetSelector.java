66e0f72d668ac60a62dea772eb08c547d41d37fb
Toon Paul <hudallap@gmail.com>
2025-10-14 17:53:25 +0000

    test

--- PetSelector.java
+++ PetSelector.java
@@ -10,7 +10,26 @@ public class PetSelector{
         if("abcdefghijklmnopqrstuvwxyz".indexOf(color.substring(i, i+1).toLowerCase()) >= 0){
             System.out.println("Enter your favorite season (winter, summer, spring, fall)");
             season = scan.NextLine();
+        } else {
+            System.out.println("Error occured! Please try again");
         }
+        if("abcdefghijklmnoprstuvwxyz".indexOf(season.substring(i, i+1).toLowerCase()) >= 0){
+            System.out.println("Enter your name: ");
+            name = scan.NextLine();
+        }
+        else {
+            System.out.println("Error occured! Please try again");
+        }
+        if("abcdefghijklmnoprstuvwxyz".indexOf(name.substring(i, i+1).toLowerCase()) >= 0){
+            System.out.println("Enter your name: ");
+            name = scan.NextLine();
+        } else {
+            System.out.println("Error occured! Please try again");
+        }
+
+        System.out.println(color);
+        System.out.println(season);
+        System.out.println(name);
         //make sure the input is valid (Idiot proof)
         //make a decision
         //tell the user waht they get
