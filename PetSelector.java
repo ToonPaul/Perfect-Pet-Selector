d07b336681496f7d2154aedfdb8981cd3f027613
Toon Paul <hudallap@gmail.com>
2025-10-14 18:28:11 +0000

    progress

--- PetSelector.java
+++ PetSelector.java
@@ -2,34 +2,35 @@ import java.util.*;
 public class PetSelector{
     public static void main (String [] args){
         //ask the user to input (name, season, and color)
-        Scanner userInput = new Scanner(System.ln);
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Enter your favorite color (red, green, or blue)");
-        String color = scan.NextLine();
+        String color = userInput.nextLine();
         String season;
         String name;
-        if("abcdefghijklmnopqrstuvwxyz".indexOf(color.substring(i, i+1).toLowerCase()) >= 0){
-            System.out.println("Enter your favorite season (winter, summer, spring, fall)");
-            season = scan.NextLine();
+        
+        if(color.toLowerCase() == "red"){
+            if("aeiou".indexOf(name.substring(0,1).toLowerCase()) >= 0){
+                System.out.println("You got a Porcupine!");
+            } else{
+                System.out.println("You got a Panda!");
+            }
+        } else if (color.toLowerCase() == "blue"){
+            if(season == "fall"){
+                //alligator
+            } else if (season == "spring"){
+                //ostrich
+            } else {
+                //axolotl
+            }
+        } else if (color.toLowerCase() == "green"){
+
         } else {
-            System.out.println("Error occured! Please try again");
-        }
-        if("abcdefghijklmnoprstuvwxyz".indexOf(season.substring(i, i+1).toLowerCase()) >= 0){
-            System.out.println("Enter your name: ");
-            name = scan.NextLine();
-        }
-        else {
-            System.out.println("Error occured! Please try again");
-        }
-        if("abcdefghijklmnoprstuvwxyz".indexOf(name.substring(i, i+1).toLowerCase()) >= 0){
-            System.out.println("Enter your name: ");
-            name = scan.NextLine();
-        } else {
-            System.out.println("Error occured! Please try again");
+            System.out.println("Invalid input, please try again");
         }
 
-        System.out.println(color);
-        System.out.println(season);
-        System.out.println(name);
+        //System.out.println(color);
+        //System.out.println(season);
+        //System.out.println(name);
         //make sure the input is valid (Idiot proof)
         //make a decision
         //tell the user waht they get
