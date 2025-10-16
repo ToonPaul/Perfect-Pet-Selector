97a96dc24a72f6e7fcd83560f8cbf2d7d0bc33ff
Toon Paul <hudallap@gmail.com>
2025-10-16 22:13:36 +0000

    whitespace

--- PetSelector.java
+++ PetSelector.java
@@ -11,7 +11,25 @@ public class PetSelector{
         String name = userInput.nextLine().toLowerCase();
         
         if((color.equals("blue")) &&(season.equals("fall"))){
-            //System.out.println(aligator);
+            System.out.println("You got an alligator");
+        } else if ((color.equals("blue")) && (season.equals("spring"))){
+            System.out.println("You got an ostrich!");
+        } else if ((color.equals("green")) && ("bcdfghjklmnpqrstvwxyz".indexOf(name.substring(0,1).toLowerCase()) >= 0) && (season.equals("winter"))){
+            System.out.println("You got a giraffe!");
+        } else if ((color.equals("green")) && !(season.equals("fall"))){
+            System.out.println("You got a dog!");
+        } else if (color.equals("red")){
+            if ("aeiou".indexOf(name.substring(0,1).toLowerCase()) >= 0){
+                System.out.println("You got a porcupine!");
+            } else {
+                System.out.println("You got a panda!");
+            }
+        } else if (season.equals("summer")){
+            System.out.println("You got a Pony!");
+        } else if (("bcdfghjklmnpqrstvwxyz".indexOf(name.substring(0,1).toLowerCase()) >= 0) && (color.equals("blue")) && !(season.equals("summer")) && !(season.equals("fall"))){
+            System.out.println("You got an axolotl!");
+        } else {
+            System.out.println("You got a rock!");
         }
         /*if(color.toLowerCase() == "red"){
             if("aeiou".indexOf(name.substring(0,1).toLowerCase()) >= 0){
