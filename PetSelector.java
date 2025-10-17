ba1adba1246a384cbde8e732ef7c3505540dd789
Toon Paul <hudallap@gmail.com>
2025-10-17 21:33:56 +0000

    final

--- PetSelector.java
+++ PetSelector.java
@@ -31,30 +31,7 @@ public class PetSelector{
         } else {
             System.out.println("You got a rock!");
         }
-        /*if(color.toLowerCase() == "red"){
-            if("aeiou".indexOf(name.substring(0,1).toLowerCase()) >= 0){
-                System.out.println("You got a Porcupine!");
-            } else{
-                System.out.println("You got a Panda!");
-            }
-        } else if (color.toLowerCase() == "blue"){
-            if(season == "fall"){
-                System.out.println("You got a alligator!");
-            } else if (season == "spring"){
-                System.out.println("You got a ostrich!");
-            } else {
-                System.out.println("You got a axolotl!");
-            }
-        } else if (color.toLowerCase() == "green"){
-            if("bcdfghjklmnpqrstvwxz".indexOf(name.substring(0,1).toLowerCase()) >= 0){
-                System.out.println("You got a giraffe!");
-            } else if((season.equals ("winter") || (season.equals("summer")) || (season.equals("spring")))){
-                System.out.println("You got a dog!");
-            }
-        } else {
-            System.out.println("You got a rock!");
-        }
-    userInput.close(); */
+    userInput.close();
     }
 
 }
